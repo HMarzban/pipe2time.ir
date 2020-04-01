b29831165a87ea5f2ad57ef28bf40320fd1820d0
hmarzban <marzban98@gmail.com>
2020-04-01 21:41:48 +0430

    fix: handle standard js lint error

--- index.js
+++ index.js
@@ -1,153 +1,157 @@
-const cheerio = require("cheerio"),
-    fs = require("fs");
+const cheerio = require("cheerio")
+const fs = require("fs")
 
-let $ = "";
-let json = {
-    version: "1.0.0",
-    creatAt: "2018/3/21",
-    cal: {
-        "1397": { jalali: "", miladi: "", qamari: "", event: [], days: [] },
-        "1398": { jalali: "", miladi: "", qamari: "", event: [], days: [] },
-        "1399": { jalali: "", miladi: "", qamari: "", event: [], days: [] },
-        "1400": { jalali: "", miladi: "", qamari: "", event: [], days: [] },
-    },
-};
+let $
+const json = {
+	version: "1.0.0",
+	creatAt: "2018/3/21",
+	cal: {
+		"1397": { jalali: "", miladi: "", qamari: "", event: [], days: [] },
+		"1398": { jalali: "", miladi: "", qamari: "", event: [], days: [] },
+		"1399": { jalali: "", miladi: "", qamari: "", event: [], days: [] },
+		"1400": { jalali: "", miladi: "", qamari: "", event: [], days: [] },
+	},
+}
 
-var persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g];
-var arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g];
+const persianNumbers = [/۰/g, /۱/g, /۲/g, /۳/g, /۴/g, /۵/g, /۶/g, /۷/g, /۸/g, /۹/g]
+const arabicNumbers = [/٠/g, /١/g, /٢/g, /٣/g, /٤/g, /٥/g, /٦/g, /٧/g, /٨/g, /٩/g]
 
-let fixNumbers = str => {
-    if (typeof str === "string") {
-        for (var i = 0; i < 10; i++) {
-            str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i);
-        }
-    }
-    return str;
-}; //fn fixNumbers
+const fixNumbers = str => {
+	if (typeof str === "string") {
+		for (let i = 0; i < 10; i++) {
+			str = str.replace(persianNumbers[i], i).replace(arabicNumbers[i], i)
+		}
+	}
+	return str
+} // fn fixNumbers
 
-const get_year = function() {
-    //FIXME: depend of this, is harmful
-    return new Promise(resolve => {
-        let year = $('input[type="text"]').val();
-        resolve(year);
-    });
-}; //fn get_year
+const getYear = function () {
+	// FIXME: depend of this, is harmful
+	return new Promise(resolve => {
+		const year = $('input[type="text"]').val()
+		resolve(year)
+	})
+} // fn getYear
 
-const get_month_days = _index => {
-    return new Promise(resolve => {
-        var montht_day = [];
-        var p = Promise.resolve();
-        $(".panel-body")
-            .eq(_index)
-            .find(".dayList > div")
-            .each((index, val) => {
-                p = p.then(function() {
-                    let jj = fixNumbers(
-                        $(val)
-                            .find(".jalali")
-                            .text(),
-                    );
-                    let mi = $(val)
-                        .find(".miladi")
-                        .text();
-                    let qa = fixNumbers(
-                        $(val)
-                            .find(".qamari")
-                            .text(),
-                    );
-                    let disable = $(val).hasClass("disabled");
-                    let holiday = $(val)
-                        .find("div")
-                        .hasClass(" holiday");
-                    montht_day.push({ holiday: holiday, disabled: disable, days: { j: jj, m: mi, q: qa } });
-                });
-            });
-        p.then(function() {
-            resolve(montht_day);
-        });
-    }); // promise return
-}; // fn get_month_days
+const getMonthDays = _index => {
+	return new Promise(resolve => {
+		var monthDay = []
+		var p = Promise.resolve()
+		$(".panel-body")
+			.eq(_index)
+			.find(".dayList > div")
+			.each((index, val) => {
+				p = p.then(function () {
+					const jj = fixNumbers(
+						$(val)
+							.find(".jalali")
+							.text(),
+					)
+					const mi = $(val)
+						.find(".miladi")
+						.text()
+					const qa = fixNumbers(
+						$(val)
+							.find(".qamari")
+							.text(),
+					)
+					const disable = $(val).hasClass("disabled")
+					const holiday = $(val)
+						.find("div")
+						.hasClass(" holiday")
+					monthDay.push({
+						holiday: holiday,
+						disabled: disable,
+						days: { j: jj, m: mi, q: qa },
+					})
+				})
+			})
+		p.then(function () {
+			resolve(monthDay)
+		})
+	}) // promise return
+} // fn getMonthDays
 
-const get_month_event = (_index = 1) => {
-    return new Promise(resolve => {
-        var p = Promise.resolve();
-        let eventDays = [];
-        $(".panel-body")
-            .eq(_index)
-            .find(".list-unstyled li")
-            .each((index, val) => {
-                p = p.then(function() {
-                    let isHoliday = $(val).hasClass("eventHoliday");
-                    let text = fixNumbers($(val).text());
-                    eventDays.push({
-                        isHoliday: isHoliday,
-                        text: text
-                            .trim()
-                            .replace(/(?:\r|\"|\n)/g, " ")
-                            .replace(/\  /g, ""),
-                    });
-                });
-            });
-        p.then(function() {
-            resolve(eventDays);
-        });
-    }); // promise return
-}; // fn get_month_event
+const getMonthEvents = (_index = 1) => {
+	return new Promise(resolve => {
+		let p = Promise.resolve()
+		const eventDays = []
+		$(".panel-body")
+			.eq(_index)
+			.find(".list-unstyled li")
+			.each((index, val) => {
+				p = p.then(function () {
+					const isHoliday = $(val).hasClass("eventHoliday")
+					const text = fixNumbers($(val).text())
+					eventDays.push({
+						isHoliday: isHoliday,
+						text: text
+							.trim()
+							.replace(/(?:\r|\n)/g, " ")
+							.replace(/\s{2}/g, ""),
+					})
+				})
+			})
+		p.then(function () {
+			resolve(eventDays)
+		})
+	}) // promise return
+} // fn getMonthEvents
 
-const get_month_heade = _index => {
-    //TODO: checke for resolve (await) callback.
-    let json;
-    let jalali = fixNumbers(
-        $(".panel-body")
-            .eq(_index)
-            .find(".header .jalali")
-            .text(),
-    );
-    let miladi = $(".panel-body")
-        .eq(_index)
-        .find(".header .miladi")
-        .text();
-    let qamari = fixNumbers(
-        $(".panel-body")
-            .eq(_index)
-            .find(".header .qamari")
-            .text(),
-    );
+const getMonthHead = _index => {
+	// TODO: checke for resolve (await) callback.
+	let json = {}
+	const jalali = fixNumbers(
+		$(".panel-body")
+			.eq(_index)
+			.find(".header .jalali")
+			.text(),
+	)
+	const miladi = $(".panel-body")
+		.eq(_index)
+		.find(".header .miladi")
+		.text()
+	const qamari = fixNumbers(
+		$(".panel-body")
+			.eq(_index)
+			.find(".header .qamari")
+			.text(),
+	)
 
-    json = { jalali, miladi, qamari };
-    return JSON.stringify(json);
-}; // fn get_month_heade
+	json = { jalali, miladi, qamari }
+	return JSON.stringify(json)
+} // fn getMonthHead
 
 const compilehtml2Jsons = async () => {
-    //TODO: match this list with _json var, To of Page
-    let year_list = ["1397", "1398", "1399", "1400"];
-    try {
-        for (let o = 0; o < year_list.length; o++) {
-            //TODO: check for if file does not exist.
-            $ = cheerio.load(fs.readFileSync("./cal/" + year_list[o] + ".html"));
+	// TODO: match this list with _json var, To of Page
+	const yearList = ["1397", "1398", "1399", "1400"]
+	try {
+		for (let o = 0; o < yearList.length; o++) {
+			// TODO: check for if file does not exist.
+			$ = cheerio.load(fs.readFileSync("./cal/" + yearList[o] + ".html"))
 
-            let year = await get_year();
+			const year = await getYear()
 
-            let month_head = JSON.parse(await get_month_heade(1));
-            json.cal[year].jalali = month_head.jalali;
-            json.cal[year].miladi = month_head.miladi;
-            json.cal[year].qamari = month_head.qamari;
+			const monthHead = JSON.parse(await getMonthHead(1))
+			json.cal[year].jalali = monthHead.jalali
+			json.cal[year].miladi = monthHead.miladi
+			json.cal[year].qamari = monthHead.qamari
 
-            for (let i = 1; i <= 12; i++) {
-                let montht_day = await get_month_days(i);
-                let month_event = await get_month_event(i);
-                json.cal[year].days.push(montht_day);
-                json.cal[year].event.push(month_event);
-            }
-        } // loop
+			for (let i = 1; i <= 12; i++) {
+				const monthDay = await getMonthDays(i)
+				const monthEvent = await getMonthEvents(i)
+				json.cal[year].days.push(monthDay)
+				json.cal[year].event.push(monthEvent)
+			}
+		} // loop
 
-        const DataJson = JSON.stringify(json);
-        fs.writeFileSync("./dist/api_calendar.json", DataJson);
-    } catch (error) {
-        console.log(error.message);
-    }
-}; // fn compilehtml2Jsons
+		const DataJson = JSON.stringify(json)
+		fs.writeFileSync("./dist/api_calendar.json", DataJson)
+	} catch (error) {
+		console.log(error.message)
+	}
+} // fn compilehtml2Jsons
 
-compilehtml2Jsons();
+compilehtml2Jsons()
 
-//FIXME: check and fix warning: "DeprecationWarning: Calling an asynchronous function without callback is deprecated"
+// FIXME: check and fix warning: "DeprecationWarning: Calling an asynchronous function without callback is deprecated"
